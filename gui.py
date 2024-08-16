055c96323f683c8ad534f52035003b188727a61f
David Zarud <davidzarud@gmail.com>
2024-08-16 15:38:44 +0300

    Start dynamic templating of cv

--- gui.py
+++ gui.py
@@ -11,11 +11,10 @@ def generate_cv():
     email = email_entry.get()
     linkedin = linkedin_entry.get()
     profile_summary = profile_summary_entry.get()
-    jobs = job_entries.get("1.0", tk.END).strip()  # Collect jobs and their descriptions from a text widget
 
     # Construct URL with all parameters
     url = (f'http://127.0.0.1:5000/generate_cv?name={name}&job_title={job_title}&location={location}'
-           f'&phone={phone}&email={email}&linkedin={linkedin}&profile_summary={profile_summary}&jobs={jobs}')
+           f'&phone={phone}&email={email}&linkedin={linkedin}&profile_summary={profile_summary}')
     webbrowser.open(url)
 
 
@@ -33,6 +32,11 @@ def main():
     global job_title_entry
     job_title_entry = tk.Entry(root)
     job_title_entry.pack(pady=5)
+
+    tk.Label(root, text="Profile Summary:").pack(pady=5)
+    global profile_summary_entry
+    profile_summary_entry = tk.Entry(root, width=50)
+    profile_summary_entry.pack(pady=5)
 
     tk.Label(root, text="Enter Location:").pack(pady=5)
     global location_entry
@@ -53,16 +57,6 @@ def main():
     global linkedin_entry
     linkedin_entry = tk.Entry(root)
     linkedin_entry.pack(pady=5)
-
-    tk.Label(root, text="Profile Summary:").pack(pady=5)
-    global profile_summary_entry
-    profile_summary_entry = tk.Entry(root, width=50)
-    profile_summary_entry.pack(pady=5)
-
-    tk.Label(root, text="Job Descriptions (each job on a new line):").pack(pady=5)
-    global job_entries
-    job_entries = tk.Text(root, height=10, width=50)
-    job_entries.pack(pady=5)
 
     tk.Button(root, text="Generate CV", command=generate_cv).pack(pady=20)
 
